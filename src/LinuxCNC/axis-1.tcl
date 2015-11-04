a779818d0859d900102208a44b4500188d1b9695
torvn77 <torvn77@mail.ru>
2015-11-04 22:43:56 +0300

    Update axis-1.tcl

--- src/LinuxCNC/axis-1.tcl
+++ src/LinuxCNC/axis-1.tcl
@@ -4,7 +4,8 @@
 
 net y-enable  <= axis.1.amp-enable-out
 net y-pos-cmd <= axis.1.motor-pos-cmd 
-net y-pos-fb  => axis.1.motor-pos-fb  
+net y-pos-fb  => axis.1.motor-pos-fb
+net y-index-enable <=> axis.1.index-enable
 
 #  ---home signals---
 net y-home-sw  => axis.1.home-sw-in
