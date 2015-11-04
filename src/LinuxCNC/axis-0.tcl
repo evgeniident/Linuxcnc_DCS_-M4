dbb9912aa1aec036591cf17808dd218c1b9f1a74
torvn77 <torvn77@mail.ru>
2015-11-04 22:45:05 +0300

    Update axis-0.tcl

--- src/LinuxCNC/axis-0.tcl
+++ src/LinuxCNC/axis-0.tcl
@@ -8,6 +8,6 @@ net x-pos-fb  => axis.0.motor-pos-fb
 net x-index-enable <=> axis.0.index-enable
 
 #  ---home signals---
-net x-home-sw => axis.0.home-sw-in
+net x-home-sw  => axis.0.home-sw-in
 net min-home-x => axis.0.home-sw-in
 net min-home-x => axis.0.neg-lim-sw-in
