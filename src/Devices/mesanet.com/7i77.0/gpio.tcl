2152adc9a85cf63198a9bb78ffb9438e6e1d778c
torvn77 <torvn77@mail.ru>
2015-11-05 08:12:14 +0300

    Update gpio.tcl

--- src/Devices/mesanet.com/7i77.0/gpio.tcl
+++ src/Devices/mesanet.com/7i77.0/gpio.tcl
@@ -6,7 +6,7 @@ net spindle-on => hm2_5i25.0.7i77.0.0.output-01
 net spindle-clean-air-cmd => hm2_5i25.0.7i77.0.0.output-02
 # Расжим цанги
 net open-collet => hm2_5i25.0.7i77.0.0.output-03
-
+# Выход сигнала Estop
 net estop-loop => hm2_5i25.0.7i77.0.0.output-00
 
 # --- Input ---
@@ -17,4 +17,9 @@ net z-home-sw   <= hm2_5i25.0.7i77.0.0.input-19-not
 net z-neg-limit <= hm2_5i25.0.7i77.0.0.input-18-not
 net x-neg-limit <= hm2_5i25.0.7i77.0.0.input-20-not
 net y-neg-limit <= hm2_5i25.0.7i77.0.0.input-22-not
+#net z-pos-limit <= hm2_5i25.0.7i77.0.0.input-18-not
+#net x-pos-limit <= hm2_5i25.0.7i77.0.0.input-20-not
+#net y-pos-limit <= hm2_5i25.0.7i77.0.0.input-22-not
+
+
 net probe-in    <= hm2_5i25.0.7i77.0.0.input-21
