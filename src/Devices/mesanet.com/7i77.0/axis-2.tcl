73a4508aeee27802f6f4cb575067fa3080076b69
torvn77 <torvn77@mail.ru>
2015-11-11 23:24:15 +0300

    Update axis-2.tcl
    Исправление ошибки в обращении к ini: S => $

--- src/Devices/mesanet.com/7i77.0/axis-2.tcl
+++ src/Devices/mesanet.com/7i77.0/axis-2.tcl
@@ -3,15 +3,15 @@
 #*******************
 loadrt pid names=pid.z
 addf pid.z.do-pid-calcs servo-thread
-setp   pid.z.Pgain     S::AXIS_2(P)
-setp   pid.z.Igain     S::AXIS_2(I)
-setp   pid.z.Dgain     S::AXIS_2(D)
-setp   pid.z.bias      S::AXIS_2(BIAS)
-setp   pid.z.FF0       S::AXIS_2(FF0)
-setp   pid.z.FF1       S::AXIS_2(FF1)
-setp   pid.z.FF2       S::AXIS_2(FF2)
-setp   pid.z.deadband  S::AXIS_2(DEADBAND)
-setp   pid.z.maxoutput S::AXIS_2(MAX_OUTPUT)
+setp   pid.z.Pgain     $::AXIS_2(P)
+setp   pid.z.Igain     $::AXIS_2(I)
+setp   pid.z.Dgain     $::AXIS_2(D)
+setp   pid.z.bias      $::AXIS_2(BIAS)
+setp   pid.z.FF0       $::AXIS_2(FF0)
+setp   pid.z.FF1       $::AXIS_2(FF1)
+setp   pid.z.FF2       $::AXIS_2(FF2)
+setp   pid.z.deadband  $::AXIS_2(DEADBAND)
+setp   pid.z.maxoutput $::AXIS_2(MAX_OUTPUT)
 
 net z-index-enable  <=>  pid.z.index-enable
 net z-enable       => pid.z.enable
@@ -26,9 +26,9 @@ net z-pos-fb       => pid.z.feedback
 
 # ---PWM Generator signals/setup---
 
-setp hm2_5i25.0.7i77.0.2.analogout1-scalemax S::AXIS_2(OUTPUT_SCALE)
-setp hm2_5i25.0.7i77.0.2.analogout1-minlim   S::AXIS_2(OUTPUT_MIN_LIMIT)
-setp hm2_5i25.0.7i77.0.2.analogout1-maxlim   S::AXIS_2(OUTPUT_MAX_LIMIT)
+setp hm2_5i25.0.7i77.0.2.analogout1-scalemax $::AXIS_2(OUTPUT_SCALE)
+setp hm2_5i25.0.7i77.0.2.analogout1-minlim   $::AXIS_2(OUTPUT_MIN_LIMIT)
+setp hm2_5i25.0.7i77.0.2.analogout1-maxlim   $::AXIS_2(OUTPUT_MAX_LIMIT)
 
 net z-output => hm2_5i25.0.7i77.0.2.analogout1
 #Эти строки должны жить здесь:
@@ -43,7 +43,7 @@ setp hm2_5i25.0.encoder.02.filter 1
 setp hm2_5i25.0.encoder.02.index-invert 0
 setp hm2_5i25.0.encoder.02.index-mask 0
 setp hm2_5i25.0.encoder.02.index-mask-invert 0
-setp hm2_5i25.0.encoder.02.scale S::AXIS_2(ENCODER_SCALE)
+setp hm2_5i25.0.encoder.02.scale $::AXIS_2(ENCODER_SCALE)
 
 net z-pos-fb <= hm2_5i25.0.encoder.02.position
 net z-vel-fb <= hm2_5i25.0.encoder.02.velocity
