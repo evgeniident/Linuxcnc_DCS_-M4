cf241dfad7867d56f00006f6956c2bc987e094b7
torvn77 <torvn77@mail.ru>
2015-11-11 23:20:36 +0300

    Update axis-0.tcl
    Исправление ошибки [lindex S:: => [lindex $::

--- src/Devices/mesanet.com/7i77.0/axis-0.tcl
+++ src/Devices/mesanet.com/7i77.0/axis-0.tcl
@@ -3,15 +3,15 @@
 #*******************
 loadrt pid names=pid.x
 addf pid.x.do-pid-calcs servo-thread
-setp   pid.x.Pgain     [lindex S::AXIS_0(P) 0]
-setp   pid.x.Igain     [lindex S::AXIS_0(I) 0]
-setp   pid.x.Dgain     [lindex S::AXIS_0(D) 0]
-setp   pid.x.bias      [lindex S::AXIS_0(BIAS) 0]
-setp   pid.x.FF0       [lindex S::AXIS_0(FF0) 0]
-setp   pid.x.FF1       [lindex S::AXIS_0(FF1) 0]
-setp   pid.x.FF2       [lindex S::AXIS_0(FF2) 0]
-setp   pid.x.deadband  [lindex S::AXIS_0(DEADBAND) 0]
-setp   pid.x.maxoutput [lindex S::AXIS_0(MAX_OUTPUT) 0]
+setp   pid.x.Pgain     [lindex $::AXIS_0(P) 0]
+setp   pid.x.Igain     [lindex $::AXIS_0(I) 0]
+setp   pid.x.Dgain     [lindex $::AXIS_0(D) 0]
+setp   pid.x.bias      [lindex $::AXIS_0(BIAS) 0]
+setp   pid.x.FF0       [lindex $::AXIS_0(FF0) 0]
+setp   pid.x.FF1       [lindex $::AXIS_0(FF1) 0]
+setp   pid.x.FF2       [lindex $::AXIS_0(FF2) 0]
+setp   pid.x.deadband  [lindex $::AXIS_0(DEADBAND) 0]
+setp   pid.x.maxoutput [lindex $::AXIS_0(MAX_OUTPUT) 0]
 
 net x-index-enable  <=>  pid.x.index-enable
 net x-enable       => pid.x.enable
@@ -26,9 +26,9 @@ net x-pos-fb       => pid.x.feedback
 
 # ---PWM Generator signals/setup---
 
-setp hm2_5i25.0.7i77.0.1.analogout0-scalemax [lindex S::AXIS_0(OUTPUT_SCALE) 0]
-setp hm2_5i25.0.7i77.0.1.analogout0-minlim   [lindex S::AXIS_0(OUTPUT_MIN_LIMIT) 0]
-setp hm2_5i25.0.7i77.0.1.analogout0-maxlim   [lindex S::AXIS_0(OUTPUT_MAX_LIMIT) 0]
+setp hm2_5i25.0.7i77.0.1.analogout0-scalemax [lindex $::AXIS_0(OUTPUT_SCALE) 0]
+setp hm2_5i25.0.7i77.0.1.analogout0-minlim   [lindex $::AXIS_0(OUTPUT_MIN_LIMIT) 0]
+setp hm2_5i25.0.7i77.0.1.analogout0-maxlim   [lindex $::AXIS_0(OUTPUT_MAX_LIMIT) 0]
 
 net x-output => hm2_5i25.0.7i77.0.1.analogout0
 #Эти строки должны жить здесь:
@@ -43,7 +43,7 @@ setp hm2_5i25.0.encoder.00.filter 1
 setp hm2_5i25.0.encoder.00.index-invert 0
 setp hm2_5i25.0.encoder.00.index-mask 0
 setp hm2_5i25.0.encoder.00.index-mask-invert 0
-setp hm2_5i25.0.encoder.00.scale [lindex S::AXIS_0(ENCODER_SCALE) 0]
+setp hm2_5i25.0.encoder.00.scale [lindex $::AXIS_0(ENCODER_SCALE) 0]
 
 net x-pos-fb <= hm2_5i25.0.encoder.00.position
 net x-vel-fb <= hm2_5i25.0.encoder.00.velocity
